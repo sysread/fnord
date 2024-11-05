53e09df9f78d86d0615325636584e2ab4d6c7224
Jeff Ober <jeffober@gmail.com>
2024-11-05 16:03:52 -0700

    quiet git warnings

--- lib/git.ex
+++ lib/git.ex
@@ -9,7 +9,12 @@ defmodule Git do
   def is_ignored?(file, git_root) do
     case System.cmd("git", ["-C", git_root, "check-ignore", file],
            stderr_to_stdout: true,
-           parallelism: true
+           parallelism: true,
+           env: [
+             {"GIT_TRACE", "0"},
+             {"GIT_CURL_VERBOSE", "0"},
+             {"GIT_DEBUG", "0"}
+           ]
          ) do
       {_, 0} -> true
       {_, _} -> false
