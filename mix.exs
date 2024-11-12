fae99caeabdf5a62a148b542337810fa9af8d03e
Jeff Ober <jeffober@gmail.com>
2024-11-11 19:08:50 -0700

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Fnord.MixProject do
   def project do
     [
       app: :fnord,
-      version: "0.4.18",
+      version: "0.4.19",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       description: "Index and search your code base with OpenAI's embeddings API",
