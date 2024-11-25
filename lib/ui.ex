819edc3e1024a19ee1a52da2a9e9376763acfd9d
Jeff Ober <jeffober@gmail.com>
2024-11-25 08:57:07 -0700

    fix bug where logger was not emitting to stderr when --quiet was implied

--- lib/ui.ex
+++ lib/ui.ex
@@ -2,48 +2,32 @@ defmodule UI do
   require Logger
 
   def flush do
-    unless quiet?() do
-      Logger.flush()
-    end
+    Logger.flush()
   end
 
   def report_step(msg, detail) do
-    unless quiet?() do
-      Logger.info(IO.ANSI.format([:green, msg, :reset, ": ", :cyan, detail, :reset], colorize?()))
-    end
+    Logger.info(IO.ANSI.format([:green, msg, :reset, ": ", :cyan, detail, :reset], colorize?()))
   end
 
   def report_step(msg) do
-    unless quiet?() do
-      Logger.info(IO.ANSI.format([:green, msg, :reset], colorize?()))
-    end
+    Logger.info(IO.ANSI.format([:green, msg, :reset], colorize?()))
   end
 
   def debug_msg(msg) do
-    unless quiet?() do
-      Logger.debug(IO.ANSI.format([:green, msg, :reset, colorize?()]))
-    end
+    Logger.debug(IO.ANSI.format([:green, msg, :reset, colorize?()]))
   end
 
   def warn(msg, detail) do
-    unless quiet?() do
-      Logger.warning(
-        IO.ANSI.format([:yellow, msg, :reset, ": ", :cyan, detail, :reset], colorize?())
-      )
-    end
+    Logger.warning(
+      IO.ANSI.format([:yellow, msg, :reset, ": ", :cyan, detail, :reset], colorize?())
+    )
   end
 
   def warn(msg) do
-    unless quiet?() do
-      Logger.warning(IO.ANSI.format([:yellow, msg, :reset], colorize?()))
-    end
+    Logger.warning(IO.ANSI.format([:yellow, msg, :reset], colorize?()))
   end
 
   defp colorize? do
     :prim_tty.isatty(:stderr)
-  end
-
-  defp quiet? do
-    Application.get_env(:fnord, :quiet, false)
   end
 end
