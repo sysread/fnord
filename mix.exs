0f3053e96174c29310e8796feec2f1682cbd8d45
Jeff Ober <jeffober@gmail.com>
2025-02-03 14:52:19 -0700

    bump version for small but important fix

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Fnord.MixProject do
   def project do
     [
       app: :fnord,
-      version: "0.6.3",
+      version: "0.6.4",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       description: "An AI powered, conversational interface for your project that learns",
