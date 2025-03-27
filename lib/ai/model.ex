a618138aefaabb529936d46ec0986c2b884efa36
Jeff Ober <jeffober@gmail.com>
2025-03-27 11:42:17 -0600

    Reduce context window on embeddings model even further

--- lib/ai/model.ex
+++ lib/ai/model.ex
@@ -56,7 +56,7 @@ defmodule AI.Model do
       reasoning: nil,
       # It's actually 8192 for this model, but this gives us a little bit of
       # wiggle room in case the tokenizer we are using falls behind.
-      context: 5000
+      context: 4000
     }
   end
 end
