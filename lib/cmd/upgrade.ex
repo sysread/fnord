cbb6da96c596e765211a1b6e52b8693621919654
Jeff Ober <jeffober@gmail.com>
2025-05-30 17:07:43 -0600

    add version check before performing upgrade

--- lib/cmd/upgrade.ex
+++ lib/cmd/upgrade.ex
@@ -21,13 +21,31 @@ defmodule Cmd.Upgrade do
 
   @impl Cmd
   def run(opts, _subcommands, _unknown) do
-    if UI.confirm("Do you want to upgrade to the latest version of fnord?", opts.yes) do
-      System.cmd("mix", ["escript.install", "--force", "github", "sysread/fnord"],
-        stderr_to_stdout: true,
-        into: IO.stream(:stdio, :line)
-      )
-    else
-      IO.puts("Cancelled")
+    Util.get_latest_version()
+    |> case do
+      {:ok, version} ->
+        current = Util.get_running_version()
+
+        if Version.compare(current, version) == :lt do
+          IO.puts("Current version: #{current}")
+          IO.puts("Latest version: #{version}")
+
+          if UI.confirm("Do you want to upgrade to the latest version of fnord?", opts.yes) do
+            System.cmd("mix", ["escript.install", "--force", "github", "sysread/fnord"],
+              stderr_to_stdout: true,
+              into: IO.stream(:stdio, :line)
+            )
+          else
+            IO.puts("Cancelled")
+          end
+        else
+          IO.puts("You are already on the latest version: #{current}")
+          false
+        end
+
+      {:error, reason} ->
+        IO.puts("Error checking for updates: #{reason}")
+        false
     end
   end
 end
