370854169b656b15cceb50ade9b6e55e853a4d41
Jeff Ober <jeffober@gmail.com>
2025-07-03 12:09:35 -0600

    remove artifact of dropped feature

--- lib/store/default_project.ex
+++
@@ -1,7 +0,0 @@
-defmodule Store.DefaultProject do
-  def store_path do
-    path = Store.store_home() |> Path.join("default")
-    if !File.exists?(path), do: File.mkdir_p!(path)
-    path
-  end
-end
