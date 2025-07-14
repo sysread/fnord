de1556485581f663e8b4a8f5bfa90c2e4e21f6d0
Jeff Ober <jeffober@gmail.com>
2025-07-14 10:11:00 -0600

    Reimplement Once using Agent

--- lib/once.ex
+++ lib/once.ex
@@ -1,30 +1,16 @@
 defmodule Once do
-  use GenServer
+  use Agent
 
-  # -----------------------------------------------------------------------------
-  # Public API
-  # -----------------------------------------------------------------------------
-  def start_link(_) do
-    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
-  end
+  def start_link(_opts), do: Agent.start_link(fn -> MapSet.new() end, name: __MODULE__)
 
   def warn(msg) do
-    GenServer.call(__MODULE__, {:warn, msg})
-  end
-
-  # -----------------------------------------------------------------------------
-  # GenServer Callbacks
-  # -----------------------------------------------------------------------------
-  def init(_) do
-    {:ok, %{}}
-  end
-
-  def handle_call({:warn, msg}, _from, state) do
-    if Map.get(state, msg) do
-      {:reply, :ok, state}
-    else
-      UI.warn(msg)
-      {:reply, :ok, Map.put(state, msg, true)}
-    end
+    Agent.get_and_update(__MODULE__, fn seen ->
+      if MapSet.member?(seen, msg) do
+        {:ok, seen}
+      else
+        UI.warn(msg)
+        {:ok, MapSet.put(seen, msg)}
+      end
+    end)
   end
 end
