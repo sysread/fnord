a75979e94f96b035fb05551d77de5e52e927909e
Jeff Ober <jeffober@gmail.com>
2025-08-15 15:26:14 -0600

    try gpt-5 again for some cases

--- lib/ai/model.ex
+++ lib/ai/model.ex
@@ -29,46 +29,29 @@ defmodule AI.Model do
 
   @spec smart() :: t
   def smart() do
-    %AI.Model{
-      model: "gpt-4.1",
-      context: 1_000_000,
-      reasoning: :none
-    }
+    # %AI.Model{model: "gpt-4.1", context: 1_000_000, reasoning: :none}
+    %AI.Model{model: "gpt-5", context: 400_000, reasoning: :medium}
   end
 
   @spec balanced() :: t
   def balanced() do
-    %AI.Model{
-      model: "gpt-4.1-mini",
-      context: 1_000_000,
-      reasoning: :none
-    }
+    # %AI.Model{ model: "gpt-4.1-mini", context: 1_000_000, reasoning: :none }
+    %AI.Model{model: "gpt-5-mini", context: 400_000, reasoning: :medium}
   end
 
   @spec fast() :: t
   def fast() do
-    %AI.Model{
-      model: "gpt-4.1-nano",
-      context: 1_000_000,
-      reasoning: :none
-    }
+    %AI.Model{model: "gpt-4.1-nano", context: 1_000_000, reasoning: :none}
+    # %AI.Model{model: "gpt-5-nano", context: 400_000, reasoning: :minimal}
   end
 
   @spec large_context() :: t
   def large_context() do
-    %AI.Model{
-      model: "gpt-4.1",
-      context: 1_000_000,
-      reasoning: :none
-    }
+    %AI.Model{model: "gpt-4.1", context: 1_000_000, reasoning: :none}
   end
 
   @spec reasoning(reasoning_level) :: t
   def reasoning(level) do
-    %AI.Model{
-      model: "o4-mini",
-      context: 200_000,
-      reasoning: level
-    }
+    %AI.Model{model: "o4-mini", context: 200_000, reasoning: level}
   end
 end
