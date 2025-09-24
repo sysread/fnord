4367ba376ac0a539155b62ddd602acc3ff495a3a
Jeff Ober <jeffober@gmail.com>
2025-09-24 08:39:01 -0600

    delete some dead code

--- lib/settings/approvals/regex_matcher.ex
+++ lib/settings/approvals/regex_matcher.ex
@@ -4,18 +4,6 @@ defmodule Settings.Approvals.RegexMatcher do
   """
 
   @doc """
-  Compiles a list of pattern strings into regex structs.
-
-  Returns an empty list if the input is not a list.
-  """
-  @spec compile_patterns([String.t()]) :: [Regex.t()]
-  def compile_patterns(patterns) when is_list(patterns) do
-    Enum.map(patterns, &Regex.compile!(&1, "u"))
-  end
-
-  def compile_patterns(_), do: []
-
-  @doc """
   Tests whether a single pattern string matches the given subject.
 
   Returns false if either argument is not a binary.
--- test/settings/regex_matcher_test.exs
+++ test/settings/regex_matcher_test.exs
@@ -1,14 +1,7 @@
 defmodule Settings.RegexMatcherTest do
   use Fnord.TestCase, async: false
-  alias Settings.Approvals.RegexMatcher
 
-  describe "compile_patterns/1" do
-    test "handles list and non-list input" do
-      patterns = RegexMatcher.compile_patterns(["^a+$"])
-      assert [%Regex{source: "^a+$"}] = patterns
-      assert RegexMatcher.compile_patterns(nil) == []
-    end
-  end
+  alias Settings.Approvals.RegexMatcher
 
   describe "matches?/2" do
     test "returns true for matching and false otherwise" do
