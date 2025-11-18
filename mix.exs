64300fbefab7416da03bed6ad332c2c7a845dcb4
Jeff Ober <jeffober@gmail.com>
2025-11-18 11:27:08 -0700

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Fnord.MixProject do
   def project do
     [
       app: :fnord,
-      version: "0.8.74",
+      version: "0.8.75",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       description: "AI code archaeology",
