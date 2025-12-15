d7f8faa18750b4d8e86d5693378b75e67e832f30
Jeff Ober <jeffober@gmail.com>
2025-12-15 09:10:16 -0700

    fix spurious auto-approval warning banner display when auto-approval is not set

--- lib/cmd/ask.ex
+++ lib/cmd/ask.ex
@@ -132,7 +132,7 @@ defmodule Cmd.Ask do
     end
 
     # Handle --auto-approve-after warning
-    if Map.has_key?(opts, :auto_approve_after) do
+    unless is_nil(opts[:auto_approve_after]) do
       UI.warning_banner("APPROVALS WILL BE GRANTED AFTER #{opts[:auto_approve_after]} SECONDS.")
       UI.warning_banner("MAY YOUR FUTURE SELF FORGIVE YOU FOR THIS DECISION.")
       UI.warning_banner("...AND MAY THE ON-CALL HAVE MERCY ON YOUR SOUL.")
