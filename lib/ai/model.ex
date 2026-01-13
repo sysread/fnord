ced2960eb3ebddec95eb26901b035492b5069275
Jeff Ober <jeffober@gmail.com>
2026-01-13 16:54:14 -0700

    I am not impressed with gpt5+. Going back to o4-mini: older, smaller context, but WAY more effective in practice.

--- lib/ai/model.ex
+++ lib/ai/model.ex
@@ -34,13 +34,11 @@ defmodule AI.Model do
     }
   end
 
-  def smart(), do: gpt5_mini(:medium)
-  def balanced(), do: gpt5_mini(:low)
+  def smart(), do: o4_mini(:medium)
+  def balanced(), do: o4_mini(:low)
   def fast(), do: gpt41_nano()
-
-  def coding(), do: o4_mini(:medium)
+  def coding(), do: smart()
   def web_search(), do: gpt_4o_mini_search_preview()
-
   def large_context(), do: large_context(:smart)
   def large_context(:smart), do: gpt41()
   def large_context(:balanced), do: gpt41_mini()
@@ -52,10 +50,8 @@ defmodule AI.Model do
   def gpt5(reasoning \\ :medium), do: new("gpt-5.1", 400_000, reasoning)
   def gpt5_mini(reasoning \\ :medium), do: new("gpt-5-mini", 400_000, reasoning)
   def gpt5_nano(reasoning \\ :medium), do: new("gpt-5-nano", 400_000, reasoning)
-
   def o4_mini(reasoning \\ :medium), do: new("o4-mini", 200_000, reasoning)
   def gpt_4o_mini_search_preview(), do: new("gpt-4o-mini-search-preview", 128_000, :none)
-
   def gpt41(), do: new("gpt-4.1", 1_000_000, :none)
   def gpt41_mini(), do: new("gpt-4.1-mini", 1_000_000, :none)
   def gpt41_nano(), do: new("gpt-4.1-nano", 1_000_000, :none)
