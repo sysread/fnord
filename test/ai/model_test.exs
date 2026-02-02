c5bebf537b6bd6e3b34b9eaf4bb7fbfd97b4eeea
Jeff Ober <jeffober@gmail.com>
2026-02-02 11:15:38 -0700

    mix format

--- test/ai/model_test.exs
+++ test/ai/model_test.exs
@@ -42,6 +42,7 @@ defmodule AI.ModelTest do
 
     assert %Model{model: "o4-mini", reasoning: :low} = Model.o4_mini(:low)
   end
+
   describe "with_reasoning/2" do
     test "nil reasoning does nothing" do
       m = Model.new("x", 1, :low)
@@ -66,7 +67,9 @@ defmodule AI.ModelTest do
     test "unknown string raises ArgumentError" do
       m = Model.new("x", 1, :low)
       # Choose a string that is extremely unlikely to exist as an atom
-      assert_raise ArgumentError, fn -> Model.with_reasoning(m, "__definitely_not_a_reasoning_level__") end
+      assert_raise ArgumentError, fn ->
+        Model.with_reasoning(m, "__definitely_not_a_reasoning_level__")
+      end
     end
   end
 end
