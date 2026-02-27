1b438d9106f2ed899ddd45edd8da3fd81e61b743
Jeff Ober <jeffober@gmail.com>
2026-02-27 13:46:10 -0700

    Fix mid-test exit

--- test/memory/session_indexer_test.exs
+++ test/memory/session_indexer_test.exs
@@ -37,8 +37,8 @@ defmodule Memory.SessionIndexerTest do
               if is_pid(pid) and Process.alive?(pid) do
                 try do
                   GenServer.stop(Services.MemoryIndexer)
-                rescue
-                  _ -> :ok
+                catch
+                  :exit, _ -> :ok
                 end
               end
             end)
